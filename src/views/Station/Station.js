86f824a4d560fca89708fc3144a9308bff67fc0b
osereda <osereda.dev@gmail.com>
2021-01-28 01:16:44 +0200

    added new logic

--- src/views/Station/Station.js
+++ src/views/Station/Station.js
@@ -23,22 +23,9 @@ const useRowStyles = makeStyles({
     },
 });
 
-function createData(n, stid, loc) {
-    return {
-        n,
-        stid,
-        loc,
-        history: [
-            { date: '2020-01-05', customerId: '11091700', amount: 3 , charge: 3 },
-            { date: '2020-01-02', customerId: 'Anonymous', amount: 1 , charge: 3 },
-            { date: '2020-01-02', customerId: 'Anonymous', amount: 1 , charge: 3 },
-        ],
-    };
-}
-
 function Row(props) {
     const { row } = props;
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(false);
     const classes = useRowStyles();
 
     return (
@@ -52,15 +39,18 @@ function Row(props) {
                 <TableCell component="th" scope="row">
                     {row.n}
                 </TableCell>
-                <TableCell align="center">{row.stid}</TableCell>
-                <TableCell align="center">{row.loc}</TableCell>
+                <TableCell align="center">{row.st_id}</TableCell>
+                <TableCell align="center">{row.st_status}</TableCell>
+                <TableCell align="center">{row.st_counts_slot}</TableCell>
+                <TableCell align="center">{row.location}</TableCell>
             </TableRow>
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
                     <Collapse in={open} timeout="auto" unmountOnExit>
                         <Box margin={1}>
                             <Typography variant="h6" gutterBottom component="div">
-                                Slot
+                                Slots
+                                <hr/>
                             </Typography>
                             <Table size="small" aria-label="purchases">
                                 <TableHead>
@@ -72,14 +62,14 @@ function Row(props) {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {row.history.map((historyRow) => (
-                                        <TableRow key={historyRow.date}>
+                                    {row.arr_slots.map((slotRow) => (
+                                        <TableRow key={slotRow.slot_id}>
                                             <TableCell component="th" scope="row">
-                                                {historyRow.date}
+                                                {slotRow.slot_id}
                                             </TableCell>
-                                            <TableCell>{historyRow.customerId}</TableCell>
-                                            <TableCell align="right">{historyRow.amount}</TableCell>
-                                            <TableCell align="right">{historyRow.charge}</TableCell>
+                                            <TableCell>{slotRow.slot_info}</TableCell>
+                                            <TableCell align="right">{slotRow.slot_status}</TableCell>
+                                            <TableCell align="right">{slotRow.slot_power}</TableCell>
                                         </TableRow>
                                     ))}
                                 </TableBody>
@@ -94,10 +84,12 @@ function Row(props) {
 
 Row.propTypes = {
     row: PropTypes.shape({
-        calories: PropTypes.number.isRequired,
-        carbs: PropTypes.number.isRequired,
-        fat: PropTypes.number.isRequired,
-        history: PropTypes.arrayOf(
+        n: PropTypes.number.isRequired,
+        st_id: PropTypes.number.isRequired,
+        st_status: PropTypes.number.isRequired,
+        st_counts_slot: PropTypes.number.isRequired,
+        loc: PropTypes.number.isRequired,
+        arr_slots: PropTypes.arrayOf(
             PropTypes.shape({
                 amount: PropTypes.number.isRequired,
                 customerId: PropTypes.string.isRequired,
@@ -111,30 +103,78 @@ Row.propTypes = {
     }).isRequired,
 };
 
-const rows = [
-    createData('1', 159, 'Anonymous dsdsds , dsdsdsdsd, dsdsdsd'),
-    createData('2', 237, 9.0),
-    createData('3', 262, 16.0),
-];
 
-export default function Station() {
-    return (
-        <TableContainer component={Paper}>
-            <Table aria-label="collapsible table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell />
-                        <TableCell>№</TableCell>
-                        <TableCell align="center">Station ID</TableCell>
-                        <TableCell align="center">Location</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <Row key={row.name} row={row} />
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+export default class Station extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+            rows2:[],
+            rows:[{ n: 100, st_id: 1000, st_status:"ok", location: "21212", st_counts_slot: 77,
+                    arr_slots: [
+                        { slot_id: '2020-01-05', slot_power: '11091700', slot_status: "ok" , slot_info: 3 },
+                        { slot_id: '2020-01-02', slot_power: '3333333', slot_status: "jr" , slot_info: 3 }
+                    ]
+            }
+            ]
+        }
+
+        this.GetStationData = this.GetStationData.bind(this);
+        this.setStationData = this.setStationData.bind(this);
+    }
+
+
+    GetStationData() {
+        fetch('http://localhost:5000/api/station/all')
+            .then(response => {
+                if (!response.ok) {
+                    console.log('error');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                if(data.length > 0) {
+                    console.log('data ->' + data);
+                    this.setStationData(data)
+                }
+            })
+            .then( setTimeout(this.GetStationData, 1000))
+    }
+
+    setStationData(data) {
+        data.forEach((item, i) => {
+            item.n = ++i;
+            item.st_counts_slot = item.id_slots.length;
+            item.st_status = "online";
+        })
+        this.setState({ rows: data})
+    }
+
+    componentDidMount() {
+        this.GetStationData();
+    }
+
+    render() {
+        return (
+            <TableContainer component={Paper}>
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell/>
+                            <TableCell>№</TableCell>
+                            <TableCell align="center">Station ID</TableCell>
+                            <TableCell align="center">Status</TableCell>
+                            <TableCell align="center">Count of slots</TableCell>
+                            <TableCell align="center">Location</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.rows.map((row) => (
+                            <Row key={row.n} row={row}/>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        );
+    }
 }
