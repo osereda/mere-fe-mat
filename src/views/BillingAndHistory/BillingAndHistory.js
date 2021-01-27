d007a4f1b9ba8bc0156153742f63d55129ed83e8
osereda <osereda.dev@gmail.com>
2021-01-27 20:09:35 +0200

    added new logic

--- src/views/BillingAndHistory/BillingAndHistory.js
+++ src/views/BillingAndHistory/BillingAndHistory.js
@@ -21,6 +21,13 @@ import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
 
+import './bah.css';
+import GridItem from "../../components/Grid/GridItem";
+import Card from "../../components/Card/Card";
+import CardHeader from "../../components/Card/CardHeader";
+import CardBody from "../../components/Card/CardBody";
+import GridContainer from "../../components/Grid/GridContainer";
+
 function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs, protein };
 }
@@ -68,11 +75,13 @@ function stableSort(array, comparator) {
 }
 
 const headCells = [
-    { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-    { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-    { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-    { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-    { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+    { id: 'sc_id', numeric: false, disablePadding: true, label: 'ID' },
+    { id: 'sc_type', numeric: true, disablePadding: false, label: 'Type' },
+    { id: 'sc_pow', numeric: true, disablePadding: false, label: 'Charge level, %' },
+    { id: 'sc_status', numeric: true, disablePadding: false, label: 'Status' },
+    { id: 'sc_perm', numeric: true, disablePadding: false, label: 'Permission' },
+    { id: 'sc_location', numeric: true, disablePadding: false, label: 'Station location' }
+
 ];
 
 function EnhancedTableHead(props) {
@@ -106,7 +115,7 @@ function EnhancedTableHead(props) {
                         >
                             {headCell.label}
                             {orderBy === headCell.id ? (
-                                <span className={classes.visuallyHidden}>
+                                <span className="visuallyHidden">
                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                 </span>
                             ) : null}
@@ -164,7 +173,7 @@ const EnhancedTableToolbar = (props) => {
                 </Typography>
             ) : (
                 <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-                    Nutrition
+                    Billing And History
                 </Typography>
             )}
 
@@ -189,166 +198,195 @@ EnhancedTableToolbar.propTypes = {
     numSelected: PropTypes.number.isRequired,
 };
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-    },
-    paper: {
-        width: '100%',
-        marginBottom: theme.spacing(2),
-    },
-    table: {
-        minWidth: 750,
-    },
-    visuallyHidden: {
-        border: 0,
-        clip: 'rect(0 0 0 0)',
-        height: 1,
-        margin: -1,
-        overflow: 'hidden',
-        padding: 0,
-        position: 'absolute',
-        top: 20,
-        width: 1,
-    },
-}));
+export default class BillingAndHistory extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+            order: 'asc',
+            orderBy: 'calories',
+            selected: [],
+            selected1: [],
+            page: 0,
+            dense: false,
+            rowsPerPage: 5,
+            rows: [{ sc_id: 1, sc_type: 'Snow', sc_pow: 'Jon', sc_status: 35, sc_location: 'ccc gggg' }],
+        };
 
-export default function BillingAndHistory() {
-    const classes = useStyles();
-    const [order, setOrder] = React.useState('asc');
-    const [orderBy, setOrderBy] = React.useState('calories');
-    const [selected, setSelected] = React.useState([]);
-    const [page, setPage] = React.useState(0);
-    const [dense, setDense] = React.useState(false);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+        this.handleClick = this.handleClick.bind(this);
+        this.setScooterData = this.setScooterData.bind(this);
+        this.getScooterData = this.getScooterData.bind(this);
+    }
 
-    const handleRequestSort = (event, property) => {
-        const isAsc = orderBy === property && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
+    handleRequestSort = (event, property) => {
+        const isAsc = this.orderBy === property && this.order === 'asc';
+        this.setState({ order: isAsc ? 'desc' : 'asc'})
+        this.setState({ orderBy: property})
     };
 
-    const handleSelectAllClick = (event) => {
+    handleSelectAllClick = (event) => {
         if (event.target.checked) {
-            const newSelecteds = rows.map((n) => n.name);
-            setSelected(newSelecteds);
+            const newSelecteds = this.state.rows.map((n) => n.name);
+            this.setState({ selected: newSelecteds})
             return;
         }
-        setSelected([]);
+        this.setState({ selected: []})
     };
 
-    const handleClick = (event, name) => {
-        const selectedIndex = selected.indexOf(name);
+    handleClick = (event, name) => {
+        const selectedIndex = this.selected.indexOf(name);
         let newSelected = [];
 
         if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, name);
+            newSelected = newSelected.concat(this.selected, name);
         } else if (selectedIndex === 0) {
-            newSelected = newSelected.concat(selected.slice(1));
-        } else if (selectedIndex === selected.length - 1) {
-            newSelected = newSelected.concat(selected.slice(0, -1));
+            newSelected = newSelected.concat(this.selected.slice(1));
+        } else if (selectedIndex === this.selected.length - 1) {
+            newSelected = newSelected.concat(this.selected.slice(0, -1));
         } else if (selectedIndex > 0) {
             newSelected = newSelected.concat(
-                selected.slice(0, selectedIndex),
-                selected.slice(selectedIndex + 1),
+                this.selected.slice(0, selectedIndex),
+                this.selected.slice(selectedIndex + 1),
             );
         }
 
-        setSelected(newSelected);
+        this.setState({ selected: newSelected})
     };
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
+    handleChangePage = (event, newPage) => {
+        this.setState({ page: newPage})
     };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
+    handleChangeRowsPerPage = (event) => {
+        this.setState({ rowsPerPage: parseInt(event.target.value, 10)})
+        this.setState({ page: 0})
     };
 
-    const handleChangeDense = (event) => {
-        setDense(event.target.checked);
+    handleChangeDense = (event) => {
+        this.setState({ dense: event.target.checked})
     };
 
-    const isSelected = (name) => selected.indexOf(name) !== -1;
+    isSelected = (name) => this.state.selected.indexOf(name) !== -1;
 
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    emptyRows = this.rowsPerPage - Math.min(this.rowsPerPage, rows.length - this.page * this.rowsPerPage);
 
-    return (
-        <div className={classes.root}>
-            <Paper className={classes.paper}>
-                <EnhancedTableToolbar numSelected={selected.length} />
-                <TableContainer>
-                    <Table
-                        className={classes.table}
-                        aria-labelledby="tableTitle"
-                        size={dense ? 'small' : 'medium'}
-                        aria-label="enhanced table"
-                    >
-                        <EnhancedTableHead
-                            classes={classes}
-                            numSelected={selected.length}
-                            order={order}
-                            orderBy={orderBy}
-                            onSelectAllClick={handleSelectAllClick}
-                            onRequestSort={handleRequestSort}
-                            rowCount={rows.length}
-                        />
-                        <TableBody>
-                            {stableSort(rows, getComparator(order, orderBy))
-                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                .map((row, index) => {
-                                    const isItemSelected = isSelected(row.name);
-                                    const labelId = `enhanced-table-checkbox-${index}`;
+    getScooterData() {
+        fetch('http://localhost:5000/api/scooter/all')
+            .then(response => {
+                if (!response.ok) {
+                    console.log('error');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                this.setScooterData(data);
+            })
+            .then( setTimeout(this.getScooterData, 1000))
+    }
 
-                                    return (
-                                        <TableRow
-                                            hover
-                                            onClick={(event) => handleClick(event, row.name)}
-                                            role="checkbox"
-                                            aria-checked={isItemSelected}
-                                            tabIndex={-1}
-                                            key={row.name}
-                                            selected={isItemSelected}
-                                        >
-                                            <TableCell padding="checkbox">
-                                                <Checkbox
-                                                    checked={isItemSelected}
-                                                    inputProps={{ 'aria-labelledby': labelId }}
-                                                />
-                                            </TableCell>
-                                            <TableCell component="th" id={labelId} scope="row" padding="none">
-                                                {row.name}
-                                            </TableCell>
-                                            <TableCell align="right">{row.calories}</TableCell>
-                                            <TableCell align="right">{row.fat}</TableCell>
-                                            <TableCell align="right">{row.carbs}</TableCell>
-                                            <TableCell align="right">{row.protein}</TableCell>
-                                        </TableRow>
-                                    );
-                                })}
-                            {emptyRows > 0 && (
-                                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
-                                    <TableCell colSpan={6} />
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onChangePage={handleChangePage}
-                    onChangeRowsPerPage={handleChangeRowsPerPage}
-                />
-            </Paper>
-            <FormControlLabel
-                control={<Switch checked={dense} onChange={handleChangeDense} />}
-                label="Dense padding"
-            />
-        </div>
-    );
+    setScooterData(data) {
+        data.forEach((item, i) => {
+            item.id = i;
+        })
+        this.setState({ rows: data})
+    }
+
+    componentDidMount() {
+        this.getScooterData();
+    }
+
+
+    render() {
+        return (
+            <GridContainer>
+                <GridItem xs={12} sm={12} md={12}>
+                    <Card>
+                        <CardHeader color="primary">
+                            <h4 className="cardTitleWhite">Scooters</h4>
+                            <p className="cardCategoryWhite">
+                                Scooters Information
+                            </p>
+                        </CardHeader>
+                        <CardBody>
+                        <div >
+                            <Paper>
+                                <EnhancedTableToolbar numSelected={this.state.selected.length}/>
+                                <TableContainer>
+                                    <Table
+                                        className="table"
+                                        aria-labelledby="tableTitle"
+                                        size={this.state.dense ? 'small' : 'medium'}
+                                        aria-label="enhanced table"
+                                    >
+                                        {/*<EnhancedTableHead*/}
+                                        {/*    numSelected={this.state.selected.length}*/}
+                                        {/*    order={this.state.order}*/}
+                                        {/*    orderBy={this.state.orderBy}*/}
+                                        {/*    onSelectAllClick={this.handleSelectAllClick}*/}
+                                        {/*    onRequestSort={this.handleRequestSort}*/}
+                                        {/*    rowCount={this.rows.length}*/}
+                                        {/*/>*/}
+                                        <TableBody>
+                                            {stableSort(this.state.rows, getComparator(this.state.order, this.state.orderBy))
+                                                .slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage)
+                                                .map((row, index) => {
+                                                    const isItemSelected = this.isSelected(row.name);
+                                                    const labelId = `enhanced-table-checkbox-${index}`;
+
+                                                    return (
+                                                        <TableRow
+                                                            hover
+                                                            onClick={(event) => this.handleClick(event, row.name)}
+                                                            role="checkbox"
+                                                            aria-checked={isItemSelected}
+                                                            tabIndex={-1}
+                                                            key={row.name}
+                                                            selected={isItemSelected}
+                                                        >
+                                                            <TableCell padding="checkbox">
+                                                                <Checkbox
+                                                                    checked={isItemSelected}
+                                                                    inputProps={{'aria-labelledby': labelId}}
+                                                                />
+                                                            </TableCell>
+                                                            <TableCell component="th" id={labelId} scope="row" padding="none">
+                                                                {row.sc_id}
+                                                            </TableCell>
+                                                            <TableCell align="right">{row.sc_type}</TableCell>
+                                                            <TableCell align="right">{row.sc_pow}</TableCell>
+                                                            <TableCell align="right">{row.sc_status}</TableCell>
+                                                            <TableCell align="right">{row.sc_perm}</TableCell>
+                                                            <TableCell align="right">{row.sc_location}</TableCell>
+                                                        </TableRow>
+                                                    );
+                                                })}
+                                            {this.emptyRows > 0 && (
+                                                <TableRow style={{height: (this.dense ? 33 : 53) * this.emptyRows}}>
+                                                    <TableCell colSpan={6}/>
+                                                </TableRow>
+                                            )}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 25]}
+                                    component="div"
+                                    count={this.state.rows.length}
+                                    rowsPerPage={this.state.rowsPerPage}
+                                    page={this.state.page}
+                                    onChangePage={this.handleChangePage}
+                                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                                />
+                            </Paper>
+                            <FormControlLabel
+                                control={<Switch checked={this.state.dense} onChange={this.handleChangeDense}/>}
+                                label="Dense padding"
+                            />
+                        </div>
+                        </CardBody>
+                    </Card>
+                </GridItem>
+            </GridContainer>
+        );
+    }
 }
